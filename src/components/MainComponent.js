e46d754f0822e33cbe619b61521659a857eaf640
abdullahalshawafi <abdullahadel.aam@gmail.com>
2020-08-17 22:54:56 +0200

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,20 +2,34 @@ import React, { Component } from "react";
 import Header from "./HeaderComponent";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from './ContactComponent';
 import Footer from "./FooterComponent";
-import { DISHES } from "../shared/dishes";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
   render() {
-    const HomePage = () => <Home />;
+    const HomePage = () => {
+      return (
+        <Home dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={this.state.promotions.filter(promoiton => promoiton.featured)[0]}
+          leader={this.state.leaders.filter(leader => leader.featured)[0]}
+        />
+      );
+    }
 
     return (
       <div>
@@ -27,6 +41,11 @@ class Main extends Component {
               exact
               path="/menu"
               component={() => <Menu dishes={this.state.dishes} />}
+            />
+            <Route
+              exact
+              path="/contactus"
+              component={Contact}
             />
             <Redirect to="/home" />
           </Switch>
