feebb97dbf4588aeae2d6dc3da0e662e9f356ea3
abdullahalshawafi <abdullahadel.aam@gmail.com>
2020-08-21 17:49:21 +0200

    Intro. to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,72 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./HeaderComponent";
 import Home from "./HomeComponent";
 import About from "./AboutComponent";
 import Menu from "./MenuComponent";
-import DishDetails from './DishdetailComponent';
-import Contact from './ContactComponent';
+import DishDetails from "./DishdetailComponent";
+import Contact from "./ContactComponent";
 import Footer from "./FooterComponent";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { DISHES } from "../shared/dishes";
-import { COMMENTS } from "../shared/comments";
-import { LEADERS } from "../shared/leaders";
-import { PROMOTIONS } from "../shared/promotions";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      leaders: LEADERS,
-      promotions: PROMOTIONS
-    };
-  }
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  };
+};
 
-  render() {
-    const HomePage = () => {
-      return (
-        <Home dish={this.state.dishes.filter(dish => dish.featured)[0]}
-          promotion={this.state.promotions.filter(promoiton => promoiton.featured)[0]}
-          leader={this.state.leaders.filter(leader => leader.featured)[0]}
-        />
-      );
-    }
-
-    const DishDetailsPage = (props) => {
-      return (
-        <DishDetails dish={this.state.dishes.filter(dish => dish.id.toString() === props.match.params.dishId)[0]}
-          comments={this.state.comments.filter(comment => comment.dishId.toString() === props.match.params.dishId)}
-        />
-      );
-    }
-
+function Main(p) {
+  const HomePage = () => {
     return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route excat path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
-          <Route
-            exact
-            path="/menu"
-            component={() => <Menu dishes={this.state.dishes} />}
-          />
-          <Route
-            path="/menu/:dishId"
-            component={DishDetailsPage}
-          />
-          <Route
-            exact
-            path="/contactus"
-            component={Contact}
-          />
-          <Redirect to="/home" />
-        </Switch>
-        <Footer />
-      </div>
+      <Home
+        dish={p.dishes.filter((dish) => dish.featured)[0]}
+        promotion={p.promotions.filter((promoiton) => promoiton.featured)[0]}
+        leader={p.leaders.filter((leader) => leader.featured)[0]}
+      />
     );
-  }
+  };
+
+  const DishDetailsPage = (props) => {
+    return (
+      <DishDetails
+        dish={
+          p.dishes.filter(
+            (dish) => dish.id.toString() === props.match.params.dishId
+          )[0]
+        }
+        comments={p.comments.filter(
+          (comment) => comment.dishId.toString() === props.match.params.dishId
+        )}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Route
+          excat
+          path="/aboutus"
+          component={() => <About leaders={p.leaders} />}
+        />
+        <Route
+          exact
+          path="/menu"
+          component={() => <Menu dishes={p.dishes} />}
+        />
+        <Route path="/menu/:dishId" component={DishDetailsPage} />
+        <Route exact path="/contactus" component={Contact} />
+        <Redirect to="/home" />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
